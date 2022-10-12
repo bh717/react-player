99126ea9ce0bfaa9d841d02adf6cf95f2bee0fa4
WebDev717 <blackhorse.cryptodev@gmail.com>
2022-10-12 13:53:30 -0700

    fix video to react-player

--- src/App.tsx
+++ src/App.tsx
@@ -18,26 +18,26 @@ function App() {
   //   });
   // }, []);
 
-  // return (
-  //   <div className="App">
-  //     <ReactPlayer
-  //       playing={true}
-  //       url={source} // video location
-  //       controls // gives the front end video controls
-  //       width="100%"
-  //       height="100%"
-  //     />
-  //   </div>
-  // );
-
   return (
-    <div>
-      <video id="video" width="1280" height="720" controls>
-        Your browser does not support the video tag.
-        <source src={source} type="video/mp4" />
-      </video>
+    <div className="App">
+      <ReactPlayer
+        playing={true}
+        url={source} // video location
+        controls // gives the front end video controls
+        width="100%"
+        height="100%"
+      />
     </div>
   );
+
+  // return (
+  //   <div>
+  //     <video id="video" width="1280" height="720" controls>
+  //       Your browser does not support the video tag.
+  //       <source src={source} type="video/mp4" />
+  //     </video>
+  //   </div>
+  // );
 }
 
 export default App;
