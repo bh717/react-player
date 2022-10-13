849937d2f283a2a64b809229248a9c38c9034126
WebDev717 <blackhorse.cryptodev@gmail.com>
2022-10-12 18:11:57 -0700

    fix react-player to video

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-import ReactPlayer from "react-player";
-import AWS from "aws-sdk";
+// import ReactPlayer from "react-player";
+// import AWS from "aws-sdk";
 
 function App() {
   // const [source, setSource] = useState<any>();
   const [source, setSource] = useState("https://react-player-backend.herokuapp.com/api/annotate");
-  const [playing, setPlaying] = useState(false);
+  // const [playing, setPlaying] = useState(false);
 
   // useEffect(() => {
   //   const endpointUrl = "api/annotate";
@@ -18,26 +18,26 @@ function App() {
   //   });
   // }, []);
 
-  return (
-    <div className="App">
-      <ReactPlayer
-        playing={true}
-        url={source} // video location
-        controls // gives the front end video controls
-        width="100%"
-        height="100%"
-      />
-    </div>
-  );
-
   // return (
-  //   <div>
-  //     <video id="video" width="1280" height="720" controls>
-  //       Your browser does not support the video tag.
-  //       <source src={source} type="video/mp4" />
-  //     </video>
+  //   <div className="App">
+  //     <ReactPlayer
+  //       playing={true}
+  //       url={source} // video location
+  //       controls // gives the front end video controls
+  //       width="100%"
+  //       height="100%"
+  //     />
   //   </div>
   // );
+
+  return (
+    <div> 
+      <video id="video" width="1280" height="720" controls loop muted autoPlay>
+        Your browser does not support the video tag.
+        <source src={source} type="video/mp4" />
+      </video>
+    </div>
+  );
 }
 
 export default App;
