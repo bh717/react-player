0ec271d65b3fe7cb8cd15cbfce2f6661f28761c6
WebDev717 <blackhorse.cryptodev@gmail.com>
2022-10-12 23:48:56 -0700

    change the video

--- src/App.tsx
+++ src/App.tsx
@@ -56,7 +56,8 @@ function App() {
       <video width="100%" height="100%" autoPlay loop muted playsInline>
         Your browser does not support the video tag.
         {/* <source src="https://elasticbeanstalk-us-east-2-045749248414.s3.amazonaws.com/1665446699465" type="video/mp4" /> */}
-        <source src="./Firstview_high.mp4" type="video/mp4" />
+        {/* <source src="./Firstview_high.mp4" type="video/mp4" /> */}
+        <source src="./video.mp4" type="video/mp4" />
       </video>
     </div>
   );
