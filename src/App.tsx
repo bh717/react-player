9cc1b1a5e547539ae088c2fd28109df3ad939eb0
WebDev717 <blackhorse.cryptodev@gmail.com>
2022-10-13 00:30:24 -0700

    fix video style

--- src/App.tsx
+++ src/App.tsx
@@ -19,48 +19,48 @@ function App() {
   //   });
   // }, []);
 
-  // return (
-  //   <div className="App">
-  //     <ReactPlayer
-  //         // style={{ opacity: loaded ? 1 : 0 }}
-  //         url={source}
-  //         muted={true}
-  //         playing={true}
-  //         controls={false}
-  //         loop={true}
-  //         width="100%"
-  //         height="100%"
-  //         playsinline={true}
-  //         onStart={() => setLoaded(true)}
-  //         config={{
-  //           youtube: {
-  //             playerVars: {
-  //               showinfo: 0,
-  //               autoplay: 0,
-  //               color: "white",
-  //               enablejsapi: 1,
-  //               disablekb: 1,
-  //               wmode: "transparent",
-  //               modestbranding: 1,
-  //               iv_load_policy: 3,
-  //             },
-  //           },
-  //         }}
-  //       />
-
-  //   </div>
-  // );
-
   return (
-    <div> 
-      <video width="100%" height="100%" autoPlay loop muted playsInline>
-        Your browser does not support the video tag.
-        {/* <source src="https://elasticbeanstalk-us-east-2-045749248414.s3.amazonaws.com/1665446699465" type="video/mp4" /> */}
-        {/* <source src="./Firstview_high.mp4" type="video/mp4" /> */}
-        <source src={source} type="video/mp4" />
-      </video>
+    <div className="App">
+      <ReactPlayer
+          // style={{ opacity: loaded ? 1 : 0 }}
+          url={source}
+          muted={true}
+          playing={true}
+          controls={false}
+          loop={true}
+          width="100%"
+          height="100%"
+          playsinline={true}
+          onStart={() => setLoaded(true)}
+          config={{
+            youtube: {
+              playerVars: {
+                showinfo: 0,
+                autoplay: 0,
+                color: "white",
+                enablejsapi: 1,
+                disablekb: 1,
+                wmode: "transparent",
+                modestbranding: 1,
+                iv_load_policy: 3,
+              },
+            },
+          }}
+        />
+
     </div>
   );
+
+  // return (
+  //   <div> 
+  //     <video width="100%" height="100%" autoPlay loop muted playsInline>
+  //       Your browser does not support the video tag.
+  //       {/* <source src="https://elasticbeanstalk-us-east-2-045749248414.s3.amazonaws.com/1665446699465" type="video/mp4" /> */}
+  //       {/* <source src="./Firstview_high.mp4" type="video/mp4" /> */}
+  //       <source src={source} type="video/mp4" />
+  //     </video>
+  //   </div>
+  // );
 }
 
 export default App;
