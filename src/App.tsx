bf227d168c2fde6f077651233b899bdb8a0c11cf
WebDev717 <blackhorse.cryptodev@gmail.com>
2022-10-12 18:29:57 -0700

    change video to react-player

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-// import ReactPlayer from "react-player";
+import ReactPlayer from "react-player";
 // import AWS from "aws-sdk";
 
 function App() {
@@ -18,26 +18,26 @@ function App() {
   //   });
   // }, []);
 
-  // return (
-  //   <div className="App">
-  //     <ReactPlayer
-  //       playing={true}
-  //       url={source} // video location
-  //       controls // gives the front end video controls
-  //       width="100%"
-  //       height="100%"
-  //     />
-  //   </div>
-  // );
-
   return (
-    <div> 
-      <video id="video" width="1280" height="720" controls loop muted autoPlay>
-        Your browser does not support the video tag.
-        <source src={source} type="video/mp4" />
-      </video>
+    <div className="App">
+      <ReactPlayer
+        playing={true}
+        url={source} // video location
+        controls // gives the front end video controls
+        width="100%"
+        height="100%"
+      />
     </div>
   );
+
+  // return (
+  //   <div> 
+  //     <video id="video" width="1280" height="720" controls loop muted autoPlay>
+  //       Your browser does not support the video tag.
+  //       <source src={source} type="video/mp4" />
+  //     </video>
+  //   </div>
+  // );
 }
 
 export default App;
