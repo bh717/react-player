cc7e0cda9d8aefaf9787ce2e3f17747fcd6adc52
WebDev717 <blackhorse.cryptodev@gmail.com>
2022-10-12 23:00:48 -0700

    fix react-player attributes

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import ReactPlayer from "react-player";
 function App() {
   // const [source, setSource] = useState<any>();
   const [source, setSource] = useState("https://react-player-backend.herokuapp.com/api/annotate");
+  const [loaded, setLoaded] = useState(false);
   // const [playing, setPlaying] = useState(false);
 
   // useEffect(() => {
@@ -18,27 +19,45 @@ function App() {
   //   });
   // }, []);
 
-  // return (
-  //   <div className="App">
-  //     <ReactPlayer
-  //       playing={true}
-  //       url={source} // video location
-  //       controls // gives the front end video controls
-  //       width="100%"
-  //       height="100%"
-  //     />
-  //   </div>
-  // );
-
   return (
-    <div> 
-      <video width="100%" height="100%" autoPlay loop muted playsInline>
-        Your browser does not support the video tag.
-        <source src="https://elasticbeanstalk-us-east-2-045749248414.s3.amazonaws.com/1665446699465" type="video/mp4" />
-      </video>
-    </div>
+    <div className="App">
+      <ReactPlayer
+          style={{ opacity: loaded ? 1 : 0 }}
+          url={source}
+          muted={true}
+          playing={true}
+          controls={false}
+          loop={true}
+          playsinline={true}
+          onStart={() => setLoaded(true)}
+          config={{
+            youtube: {
+              playerVars: {
+                showinfo: 0,
+                autoplay: 0,
+                color: "white",
+                enablejsapi: 1,
+                disablekb: 1,
+                wmode: "transparent",
+                modestbranding: 1,
+                iv_load_policy: 3,
+              },
+            },
+          }}
+        />
 
+    </div>
   );
+
+  // return (
+  //   <div> 
+  //     <video width="100%" height="100%" autoPlay loop muted playsInline>
+  //       Your browser does not support the video tag.
+  //       <source src="https://elasticbeanstalk-us-east-2-045749248414.s3.amazonaws.com/1665446699465" type="video/mp4" />
+  //     </video>
+  //   </div>
+
+  // );
 }
 
 export default App;
